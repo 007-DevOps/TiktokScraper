f0f15aadf52749a3be536cc15450cf91f52a28fe
Leinnner <devleiner@gmail.com>
2023-07-03 19:14:55 -0500

    Add tiktok-scrapper-region

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 import { Actor } from "apify";
 import { TTScraper  } from "tiktok-scraper-ts";
+import { getUserRegion } from './src/tiktok-scrapper-region.js';
 
 const TikTokScraper = new TTScraper();
-
 
 (async () => {
   await Actor.init();
   const { user } = await Actor.getInput();
 
   const fetchUser = await TikTokScraper.user(user);
-  console.log(fetchUser);
-  await Actor.pushData({ fetchUser });
+  
 
+  try {
+    const region = await getUserRegion(user);
+    if (region) {
+      fetchUser.region = region;
+    } else {
+      return `Failed to get user region ${user}`;
+    }
+  } catch (error) {
+    console.error('Error getting region:', error);
+  }
+
+  const userWithRegion = { ...fetchUser }; 
+  userWithRegion.region = fetchUser.region; 
+
+  console.log(userWithRegion);
+  await Actor.pushData({ fetchUser });
   await Actor.exit();
 })();
